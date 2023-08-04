a1b7cc70606d8cc57ec54875051fb794878a0645
Zhoomart <jabdykasymov@gmail.com>
2023-08-04 16:17:19 +0600

    1

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -1,10 +1,11 @@
 ﻿Console.Clear();
 //Console.SetCursorPosition(10, 4); //10 символов от левого края отступ, 4 от верхнего
 //Console.WriteLine("+");
+Console.SetWindowSize(100, 100);
 
-int xa = 30, ya = 1;
-int xb = 55,  yb = 100; 
-int xc = 80, yc = 20;
+int xa = 40, ya = 1;
+int xb = 1,  yb = 30; 
+int xc = 80, yc = 30;
   
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
@@ -18,7 +19,7 @@ Console.WriteLine("+");
 int x = xa, y = xb;
 int count = 0;
 
-while (count < 100);
+while (count < 10000)
 {
   int what = new Random().Next(0, 3);
   if (what == 0)
