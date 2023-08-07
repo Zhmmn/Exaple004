08d9c9b3e71775ab4174a1b4e7d8e0bd80d799c9
Zhoomart <jabdykasymov@gmail.com>
2023-08-08 00:15:21 +0600

    MetodArray

---
+++ Example010_MetodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {12,15,18,9,5,55,8,6,3, 55};
+
+int n = array.Length;
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int index = 0;
+
+while (index < n) 
+{
+  if (array[index] == number)
+  {
+    Console.WriteLine(index);
+    break;
+  }
+  index++;
+}
